f6dd7476054570f7e06b30dafe88e46dc3d5711e
Максим Филисов <avengernaxmf@gmail.com>
2023-08-12 14:57:39 +0300

    creating

---
+++ stack implementation/stack implementation.cpp
@@ -0,0 +1,9 @@
+﻿// stack implementation.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
